d2778b0b09da4d298dd7e59115b98b1a7a3c3d0c
kotaro <turingsaketi@gmail.com>
2018-11-22 16:49:51 +0900

    file change

--- Fapp/menter/urls.py
+++ Fapp/menter/urls.py
@@ -3,17 +3,17 @@ from . import views
 
 urlpatterns = [
 #トップページ
-url(r'^$', views.index, name = 'index'),
+url(r'^$', views.index, name='index'),
 #メンター
-url(r'^menter$', views.menter, name = 'menter'),
+url(r'^menter$', views.menter, name='menter'),
 #コンタクト
-url(r'^contact$', views.contact, name = 'contact'),
+url(r'^contact$', views.contact, name='contact'),
 #コメント
-url(r'^comment$', views.comment, name = 'comment'),
+url(r'^comment$', views.comment, name='comment'),
 #アバウト
-url(r'^about$', views.about, name = 'about'),
+url(r'^about$', views.about, name='about'),
 #哲学
-url(r'^philosophy$', views.philosophy, name = 'philosophy'),
+url(r'^philosophy$', views.philosophy, name='philosophy'),
 #ディテール
 url(r'^([0-9]+)/$', views.detail, name='detail'),
 #like
